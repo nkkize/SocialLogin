07ae7abbaf156d60aaeca922463511aa460b2903
nkkize <nkk.snat@gmail.com>
2016-11-25 18:00:42 +0530

    facebook login

--- src/main/java/com/nkkize/sociallogin/SocialLoginApplication.java
+++ src/main/java/com/nkkize/sociallogin/SocialLoginApplication.java
@@ -2,20 +2,36 @@ package com.nkkize.sociallogin;
 
 import java.security.Principal;
 
+import javax.servlet.Filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
+import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
+import org.springframework.security.oauth2.client.token.grant.code.AuthorizationCodeResourceDetails;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-@EnableOAuth2Sso
+@EnableOAuth2Client
 @RestController
-
 public class SocialLoginApplication extends WebSecurityConfigurerAdapter{
+	
+	@Autowired
+	OAuth2ClientContext oauth2ClientContext;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SocialLoginApplication.class, args);
@@ -25,6 +41,7 @@ public class SocialLoginApplication extends WebSecurityConfigurerAdapter{
 	  protected void configure(HttpSecurity http) throws Exception {
 	    http
 	      .antMatcher("/**")
+	      .addFilterBefore(ssoFilter(), BasicAuthenticationFilter.class)
 	      .authorizeRequests()
 	        .antMatchers("/", "/login**", "/webjars/**")
 	        .permitAll()
@@ -37,6 +54,34 @@ public class SocialLoginApplication extends WebSecurityConfigurerAdapter{
 	@RequestMapping("/user")
 	public Principal user(Principal principal){
 		return principal;
+	}
+	
+	private Filter ssoFilter() {
+		  OAuth2ClientAuthenticationProcessingFilter facebookFilter = new OAuth2ClientAuthenticationProcessingFilter("/login/facebook");
+		  OAuth2RestTemplate facebookTemplate = new OAuth2RestTemplate(facebook(), oauth2ClientContext);
+		  facebookFilter.setRestTemplate(facebookTemplate);
+		  facebookFilter.setTokenServices(new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId()));
+		  return facebookFilter;
+		}
+	
+	@Bean
+	@ConfigurationProperties("facebook.client")
+	public AuthorizationCodeResourceDetails facebook() {
+		return new AuthorizationCodeResourceDetails();
+	}
+	
+	@Bean
+	@ConfigurationProperties("facebook.resource")
+	public ResourceServerProperties facebookResource() {
+		return new ResourceServerProperties();
+	}
+	
+	@Bean
+	public FilterRegistrationBean oauth2ClientFilterRegistration(OAuth2ClientContextFilter filter) {
+		FilterRegistrationBean registration = new FilterRegistrationBean();
+		registration.setFilter(filter);
+		registration.setOrder(-100);
+		return registration;
 	}
 
 }
